3cdce996c9a3fc8ac5567ca81195d69b821b0902
Alita Shrestha <aelistha@gmail.com>
2022-02-04 16:49:03 +0545

    Basic app set up and App icon updated

---
+++ App.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {Dimensions} from 'react-native';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import WebView from 'react-native-webview';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const iframeWidth = Math.round(Dimensions.get('window').width);
+  const iframeHeight = Math.round(Dimensions.get('window').height);
+  console.log("isDarkMode", isDarkMode)
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar hidden={true} backgroundColor={'#fff'} barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <WebView
+            originWhitelist={['*']}
+            source={{uri: 'https://v3.explorug.com/explorug.html?page=wovenedge&pageview=app'}}
+            style={{height: iframeHeight, width: iframeWidth}}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
